50cdf9c8a1dee1293c2cd8c28ff3ace70a977b08
Alexander Wilts <alexander.wilts@codecentric.de>
2020-04-17 13:41:21 +0200

    added item endpoints in backend

---
+++ src/main/kotlin/de/wilts/listigbackend/Item.kt
@@ -0,0 +1,3 @@
+package de.wilts.listigbackend
+
+data class Item(val messageId: String, val userId: String, val groupId: String, val text: String)
--- src/main/kotlin/de/wilts/listigbackend/MessageController.kt
+++ src/main/kotlin/de/wilts/listigbackend/MessageController.kt
@@ -1,14 +1,47 @@
 package de.wilts.listigbackend
 
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import com.google.cloud.datastore.Datastore
+import com.google.cloud.datastore.DatastoreOptions
+import com.google.cloud.datastore.Entity
+import com.google.cloud.datastore.Key
+import org.springframework.web.bind.annotation.*
 
-data class Message(val text: String, val priority: String)
-
+@RequestMapping("/backend")
 @RestController
 class MessageController {
-    @RequestMapping("/backend/message")
-    fun message(): Message {
-        return Message("Hello from Dat Boi", "High")
+
+    private final val testItem = Item("messageId", "myid", "groupId", "some deep message")
+    val items = mutableListOf<Item>(testItem)
+
+    @PostMapping("/items")
+    fun postItems(@RequestBody item: Item) {
+        println(item)
+        items.add(item)
+
+        val kind = "itemKind"
+        val name = "itemKeyName"
+        val taskKey: Key = datastore.newKeyFactory().setKind(kind).newKey(name)
+
+        val task: Entity = Entity.newBuilder(taskKey)
+                .set("messageId", item.messageId)
+                .set("myid", item.userId)
+                .set("groupId", item.groupId)
+                .set("text", item.text)
+                .build()
+
+        datastore.put(task)
+        System.out.printf("Saved %s: %s%n", task.key.name, task.getString("description"))
+
+        val retrieved = datastore[taskKey]
+        System.out.printf("Retrieved %s: %s%n", taskKey.name, retrieved.getString("description"))
     }
+
+    @GetMapping("/items")
+    fun getItems(): Collection<Item> {
+        println(items)
+        return items
+    }
+
+    val datastore: Datastore = DatastoreOptions.getDefaultInstance().service
+
 }
